c87c5657bca75d6021fd62ab65be5bb6cf3d9495
Maani <maani666@gmail.com>
2016-07-21 14:56:56 +0100

    added dropdowns, changed colum heights, included new icons

--- js/index.js
+++ js/index.js
@@ -1,4 +1,30 @@
+// sidebar
 $('.selected-meeting, .overlay, .meetings-col li').on('click', function(){
     $('.meetings-col').toggleClass('off');
     $( ".overlay" ).fadeToggle( "slow" );
+});
+
+// settings dropdown
+$('#settings-dropdown-toggle').on('click', function(){
+    $('#settings-dropdown').fadeToggle("fast");
+});
+
+$('.page-body').on('click', function(){
+    $('#settings-dropdown').fadeOut();
+    $('#settings-dropdown, #settings-dropdown-toggle').on('click', function(e) {  
+    e.stopPropagation();
+	});
+});
+
+
+// organisation-dropdown
+$('#organisation-dropdown-toggle').on('click', function(){
+    $('#organisation-dropdown').fadeToggle();
+});
+
+$('.page-body').on('click', function(){
+    $('#organisation-dropdown').fadeOut("fast");
+    $('#organisation-dropdown, #organisation-dropdown-toggle').on('click', function(e) {  
+    e.stopPropagation();
+	});
 });
